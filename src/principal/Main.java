b5c5431248dca955fc9ec8c4c02782f4c24ccdb4
usuario <kevinmoralesk123@gmail.com>
2026-06-24 18:12:40 -0500

    feat(principal): agregar clase Main y finalizar proyecto base

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,11 +1,18 @@
 package principal;
 
-/**
- *
- * @author usuario
- */
+import javax.swing.SwingUtilities;
+import vista.FormLogin;
+
 public class Main {
+
     public static void main(String[] args) {
-        
+
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                new FormLogin();
+            }
+        });
     }
 }
